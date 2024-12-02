49ea20c9bd01f421be56e495a7d3fc33e569a80d
bigpimplimp <levi.jcecil@gmail.com>
2024-12-02 11:55:50 -0800

    add switch page logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 // index.js
 console.log('Yerttt');
+
+(function switchPage () {
+    const homeButton = document.getElementById('home-button');
+    const menuButton = document.getElementById('menu-button');
+    const aboutButton = document.getElementById('about-button');
+    
+    const wipeDiv = (divId) => {
+        const div = document.getElementById(divId)
+        while(div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+    }
+
+    homeButton.addEventListener(click, () => {
+        wipeDiv('content');
+    })
+    
+    menuButton.addEventListener(click, () => {
+        wipeDiv('content');
+    })
+    
+    aboutButton.addEventListener(click, () => {
+        wipeDiv('content');
+    })
+})();
